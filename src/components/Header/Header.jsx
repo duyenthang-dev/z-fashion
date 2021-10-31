7eafd13fce3173cf61f137db77582bf901c938d8
duyenthang_dev <duyenthang.dev@gmail.com>
2021-10-31 11:16:57 +0700

    create project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './header.css';
+
+function Header() {
+    return (
+        <div className="header1">
+            <h2>this is header</h2>
+        </div>
+    );
+}
+export default Header;
