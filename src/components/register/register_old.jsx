8da3db6a60f74b6de01c5c42717fa9f8232f5a85
An Nguyễn <nguyenthanhan140701@gmail.com>
2021-11-15 10:38:52 +0700

    login page
    15/11

---
+++ src/components/register/register_old.jsx
@@ -0,0 +1,26 @@
+
+import "./../../utility/css/utility.css";
+import React from "react";
+import logo_img from './../../assets/images/logo/logo.svg';
+import fb_logo from  './../../assets/images/logo/fb.png';
+import gg_logo from  './../../assets/images/logo/gg.png';
+import "./register.css"
+
+
+
+
+function Register_compo()  {
+    return (
+        
+        
+        <div className="Register_compo">
+
+
+            <img src={logo_img} alt="" />
+
+          
+           
+        </div>
+    );
+};
+export default Register_compo;
