28111d33842a7b0e1f3e14ebd73e5610869c3e3a
MaiUoc-hcmut <73373065+MaiUoc-hcmut@users.noreply.github.com>
2021-11-26 20:36:37 +0700

    update checkout button

--- src/pages/checkout/bill/paymentMethod.jsx
+++ src/pages/checkout/bill/paymentMethod.jsx
@@ -5,8 +5,8 @@ import ImagePayment from './payment-image/payment-image';
 import Button from '@mui/material/Button';
 import { useHistory } from 'react-router-dom';
 
-class ImageAvatars extends React.Component {
-    render() {
+function ImageAvatars() {
+        
         return ( 
             <div className="payment-frame">
                 <h2 style={{marginBottom: 20}}>Phương thức thanh toán</h2>
@@ -31,12 +31,12 @@ class ImageAvatars extends React.Component {
                     </Grid>
                     <Grid item xs={1}>
                         <label>
-                            <input type="radio" class="option-input radio" name="paymentMethod" id="mastercard" value="Master Card" />
+                            <input type="radio" class="option-input radio" name="paymentMethod" id="mastercard" value="MasterCard" />
                         </label>
                     </Grid>
     
                     <Grid item xs={1}>
-                        <img src={ImagePayment['Visa']} alt="Momo" className="image-payment" />
+                        <img src={ImagePayment['Visa']} alt="Visa" className="image-payment" />
                     </Grid>
                     <Grid item xs={10}>
                         <p>Visa</p>
@@ -48,19 +48,19 @@ class ImageAvatars extends React.Component {
                     </Grid>
     
                     <Grid item xs={1}>
-                        <img src={ImagePayment['ViettelPay']} alt="Momo" className="image-payment" />
+                        <img src={ImagePayment['ViettelPay']} alt="ViettelPay" className="image-payment" />
                     </Grid>
                     <Grid item xs={10}>
                         <p>Viettle Pay</p>
                     </Grid>
                     <Grid item xs={1}>
                         <label>
-                            <input type="radio" class="option-input radio" name="paymentMethod" id="viettleay" value="Viettel Pay" />
+                            <input type="radio" class="option-input radio" name="paymentMethod" id="viettleay" value="ViettelPay" />
                         </label>
                     </Grid>
                 </Grid>
                 
-                <Button variant="contained" style={{marginTop: 40}} onClick={() => {
+                <button variant="contained" style={{marginTop: 40}} onClick={() => {
                     const rbs = document.getElementsByName('paymentMethod');
                     let countChecked = false;
                     let checkedOption;
@@ -68,15 +68,23 @@ class ImageAvatars extends React.Component {
                         if(rb.checked) {
                             countChecked = true;
                             checkedOption = rb.value;
+                            break;
                         }
                     }
                     if(!countChecked) alert('Please check your payment option');
-                    else alert(`Direct to ${checkedOption}`);
+                    else {
+                        if(checkedOption === "Momo") window.location.replace("https://momo.vn/");
+                        else if(checkedOption === "MasterCard") window.location.replace("https://www.mastercard.us/en-us.html");
+                        else if(checkedOption === "ViettelPay") window.location.replace("https://viettelpay.vn/");
+                        else window.location.replace("https://www.visa.com.vn/en_VN")
+                    }
                     
-                }}>THANH TOÁN</Button>
+                }}>THANH TOÁN</button>
+
+                
             </div>
         );
-    }
+    
 }
 
 export default ImageAvatars;
