a826591ca5b8bbc01fc8f57dcd538f22cc7c64b2
duyenthang_dev <duyenthang.dev@gmail.com>
2021-11-27 12:56:53 +0700

    fix login - sigup UI

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -3,82 +3,91 @@ import React from "react";
 import logo_img from "./../../assets/images/logo/logo.svg";
 import fb_logo from "./../../assets/images/logo/fb.png";
 import gg_logo from "./../../assets/images/logo/gg.png";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
 import "./login.css";
-
 function LoginCompo() {
     return (
-        <div className="Login_compo">
-            <div className="logo_zf">
-                <img
-                    src={logo_img}
-                    alt="z_fashion"
-                    width="412px"
-                    height="213px"
-                />
-            </div>
-
-            <div className="login_page">
-                <div className="Login_frame">
-                    <h1>Đăng Nhập</h1>
-
-                    <div className="login_box">
-                        <input
-                            className="username"
-                            type="text"
-                            placeholder="     Email/Số điện thoại/User name"/>
-                        <br />
-                        <input
-                            className="pass"
-                            type="password"
-                            placeholder="     Mật khẩu"
+        <Container maxWidth="lg" className="mt-5 mb-6">
+            <Grid container>
+                <Grid item lg={5}>
+                    <div className="logo_zf">
+                        <img
+                            src={logo_img}
+                            alt="z_fashion"
+                            width="60%"
+                            height="60%"
                         />
                     </div>
+                </Grid>
+                <Grid item lg={7}>
+                    <div className="login_page">
+                        <div className="Login_frame">
+                            <h1>Đăng Nhập</h1>
 
-                    <button className="login_text">Đăng nhập</button>
+                            <div className="login_box">
+                                <input
+                                    className="username"
+                                    type="text"
+                                    placeholder="Email/Số điện thoại/User name"
+                                />
+                                <br />
+                                <input
+                                    className="pass"
+                                    type="password"
+                                    placeholder="Mật khẩu"
+                                />
+                            </div>
 
-                    <br />
-                    <div className="lostpass">
-                        <a className="lostpasstext" href="/">
-                            Quên mật khẩu
-                        </a>
+                            <button className="login_text btn-login">Đăng nhập</button>
+
+                            <br />
+                            <div className="lostpass">
+                                <a className="lostpasstext" href="/">
+                                    Quên mật khẩu
+                                </a>
+                            </div>
+                        </div>
+                        <hr />
+                        <div className="Another_way_to_login">
+                            <h3 className="login2text">
+                                Hoặc có thể đăng nhập bằng
+                            </h3>
+
+                            <div className="login3rd">
+                                <a className="fb_link" href="/" target="_blank">
+                                    <img
+                                        src={fb_logo}
+                                        alt="facebook"
+                                        border="0"
+                                        width="100px"
+                                        height="20px"
+                                    />
+                                </a>
+
+                                <a className="gg_link" href="/" target="_blank">
+                                    <img
+                                        src={gg_logo}
+                                        alt="google"
+                                        border="0"
+                                        width="100px"
+                                        height="33px"
+                                    />
+                                </a>
+                            </div>
+                            <br />
+                        </div>
+
+                        <div className="SignUp">
+                            <p className="signuptext">Chưa có tài khoản?</p>
+                            <a className="signuplink" href="/register">
+                                Đăng ký
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <hr />
-                <div className="Another_way_to_login">
-                    <h3 className="login2text">Hoặc có thể đăng nhập bằng</h3>
-
-                    <div className="login3rd">
-                        <a className="fb_link" href="/" target="_blank">
-                            <img
-                                src={fb_logo}
-                                alt="facebook"
-                                border="0"
-                                width="100px"
-                                height="20px"
-                            />
-                        </a>
-
-                        <a className="gg_link" href="/" target="_blank">
-                            <img
-                                src={gg_logo}
-                                alt="google"
-                                border="0"
-                                width="100px"
-                                height="33px"
-                            />
-                        </a>
-                    </div>
-                    <br />
-                </div>
-
-                <div className="SignUp">
-                    <p className="signuptext">Chưa có tài khoản?</p>
-                    <a className="signuplink" href="/">
-                        Đăng ký
-                    </a>
-                </div>
-            </div>
-        </div>
+                </Grid>
+            </Grid>
+        </Container>
     );
 }
 export default LoginCompo;
--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,163 +1,93 @@
-import './register.css';
+import "./register.css";
 import React from "react";
 import logo_img from "./../../assets/images/logo/logo.svg";
 import fb_logo from "./../../assets/images/logo/fb.png";
 import gg_logo from "./../../assets/images/logo/gg.png";
-
-
-const Register = () =>{
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+const Register = () => {
     return (
-        <div className="Regist_compo">
-                <div className="Signup_text">Đăng Ký</div>
-
-                
-                <div className="Info_frame">
-
-                        <div className="Info_frame_left">
-
-
-                                {/* INPUT_BOX_SPLIT */}
-
-                                <div className="input_box">
-                                        <div className="input_box_left">
-                                                <h2 className="info">Số điện thoại</h2>
-                                                <h3 className="redflag">* Bắt buộc</h3>
-                                        </div>
-
-                                        <input className="input_box_right" type="text" />
-                                </div>
-
-                                {/* INPUT_BOX_SPLIT */}
-                                
-                                <div className="input_box">
-                                        <div className="input_box_left">
-                                                <h2 className="info">Tên đăng nhập</h2>
-                                                <h3 className="redflag">* Bắt buộc</h3>
-                                        </div>
-
-                                        <input className="input_box_right" type="text" />
-                                </div>
-
-                                {/* INPUT_BOX_SPLIT */}
-
-                                <div className="input_box">
-                                        <div className="input_box_left">
-                                                <h2 className="info">Mật khẩu</h2>
-                                                <h3 className="redflag">* Bắt buộc</h3>
-                                        </div>
-
-                                        <input className="input_box_right" type="text" />
-                                </div>
-
-                                {/* INPUT_BOX_SPLIT */}
-
-                                <div className="input_box">
-                                        <div className="input_box_left">
-                                                <h2 className="info">Tên</h2>
-                                                <h3 className="redflag">* Bắt buộc</h3>
-                                        </div>
-
-                                        <input className="input_box_right" type="text" />
-                                </div>
-
+        <Container maxWidth="lg">
+            <div className="Signup_text">Đăng Ký</div>
+            <Grid container>
+                <Grid item lg={6}>
+                    <div className="input_box">
+                        <div className="input_box_left">
+                            <h2 className="info">Số điện thoại</h2>
+                            <h3 className="redflag">* Bắt buộc</h3>
                         </div>
 
-
-
-
-
-
-
-
-
-
-                        <div className="Info_frame_right">
-
-                                         {/* INPUT_BOX_SPLIT */}
-
-                                 <div className="input_box">
-                                        <div className="input_box_left">
-                                                <h2 className="info">Số điện thoại</h2>
-                                                <h3 className="redflag">* Bắt buộc</h3>
-                                        </div>
-
-                                        <input className="input_box_right" type="text" />
-                                </div>
-
-                                {/* INPUT_BOX_SPLIT */}
-                                
-                                <div className="input_box">
-                                        <div className="input_box_left">
-                                                <h2 className="info">Tên đăng nhập</h2>
-                                                <h3 className="redflag">* Bắt buộc</h3>
-                                        </div>
-
-                                        <input className="input_box_right" type="text" />
-                                </div>
-
-                                {/* INPUT_BOX_SPLIT */}
-
-                                <div className="input_box">
-                                        <div className="input_box_left">
-                                                <h2 className="info">Mật khẩu</h2>
-                                                <h3 className="redflag">* Bắt buộc</h3>
-                                        </div>
-
-                                        <input className="input_box_right" type="text" />
-                                </div>
-
-                                {/* INPUT_BOX_SPLIT */}
-
-                                <div className="input_box">
-                                        <div className="input_box_left">
-                                                <h2 className="info">Tên</h2>
-                                                <h3 className="redflag">* Bắt buộc</h3>
-                                        </div>
-
-                                        <input className="input_box_right" type="text" />
-                                </div>
-
+                        <input className="input_box_right" type="text" />
+                    </div>
+                    <div className="input_box">
+                        <div className="input_box_left">
+                            <h2 className="info">Họ tên</h2>
+                            <h3 className="redflag">* Bắt buộc</h3>
                         </div>
-                        
 
+                        <input className="input_box_right" type="text" />
+                    </div>
+                    <div className="input_box">
+                        <div className="input_box_left">
+                            <h2 className="info">Email</h2>
+                            <h3 className="redflag">* Bắt buộc</h3>
+                        </div>
 
+                        <input className="input_box_right" type="email" />
+                    </div>
+                </Grid>
+                <Grid item lg={6}>
+                    <div className="input_box">
+                        <div className="input_box_left">
+                            <h2 className="info">Tên đăng nhập</h2>
+                            <h3 className="redflag">* Bắt buộc</h3>
+                        </div>
 
-                </div>
+                        <input className="input_box_right" type="text" />
+                    </div>
+                    <div className="input_box">
+                        <div className="input_box_left">
+                            <h2 className="info">Mật khẩu</h2>
+                            <h3 className="redflag">* Bắt buộc</h3>
+                        </div>
 
-                
-
-                <button className="Sign_up_text">Đăng Ký</button>
-                <hr className="hr_signin"></hr>
-                <div className="Another_way_to_login">Hoặc có thể đăng ký bằng</div>
-
-                <div className="login3rd">
+                        <input className="input_box_right" type="text" />
+                    </div>
+                </Grid>
+                <div className="Regist_compo">
+                    <button className="btn-signup">Đăng Ký</button>
+                    <hr className="hr_signin"></hr>
+                    <h3>Hoặc có thể đăng kí bằng</h3>
+                    <div className="login3rd">
                         <a className="fb_link" href="/" target="_blank">
                             <img
                                 src={fb_logo}
                                 alt="facebook"
                                 border="0"
-                                width="100px"
-                                height="20px"
+                                width="100%"
+                                height="100%"
                             />
                         </a>
-
                         <a className="gg_link" href="/" target="_blank">
                             <img
                                 src={gg_logo}
                                 alt="google"
                                 border="0"
-                                width="100px"
-                                height="33px"
+                                width="100%"
+                                height="100%"
                             />
                         </a>
                     </div>
 
                     <div className="LoginIn">
                         <p className="Login_text">Đã có tài khoản?</p>
-                        <a className="Login_link" href="/">Đăng nhập</a>
+                        <a className="Login_link" href="/login">
+                            Đăng nhập
+                        </a>
                     </div>
-
-        </div>
-    )
-}
+                </div>
+            </Grid>
+        </Container>
+    );
+};
 export default Register;
